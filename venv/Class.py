3ad746f9fcb9e5df593691c64b74da4c09abbdd3
Ranjith Kumar <37208730+ranjithkumar97@users.noreply.github.com>
2019-01-16 21:15:11 +0530

    Update Class.py

--- venv/Class.py
+++ venv/Class.py
@@ -2,3 +2,19 @@ import re
 
 txt = "The rain in Spain"
 x = re.search("^The.*Spain$", txt)
+
+def roy():
+    x=int(input('enter the 1st number'))
+    y=int(input('enter the 2nd nukmber'))
+    z=x*y
+    print(z)
+    return x,y,z
+ranjith()
+roy()
+
+def my_function(x):
+  return list( dict.fromkeys(x) )
+
+mylist = my_function(["a", "b", "a", "c", "c"])
+
+print(mylist)
